83eb90e308a6de9be868b2d118f939cf85768875
Andrey K <karway211@gmail.com>
2020-02-09 13:44:10 +0300

    init: start task

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <div>
+        <a href="#c">Profile</a>
+      </div>
+      <div>
+        <a href="#c">Messages</a>
+      </div>
+      <div>
+        <a href="#c">News</a>
+      </div>
+      <div>
+        <a href="#c">Music</a>
+      </div>
+      <div>
+        <a href="#c">Settings</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/components/Prifile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className='content'>
+        <div className="way">
+          <img src="https://st2.depositphotos.com/1046670/5401/i/450/depositphotos_54014113-stock-photo-eagle.jpg" alt="img"/>
+        </div>
+        <div className='avatar'>
+          <img src="https://2krota.ru/wp-content/uploads/2019/02/0_i-1.jpg" alt="img"/> + descr
+        </div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 1
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Profile;
